4cd4bc721dfc2776665f9fea63cc8d688b967674
shsun <shsun@shsuns-MacBook-Pro.local>
2017-12-08 20:47:13 +0800

    init

--- dubbo_spring_demo_1/kuaibao-consumer/src/main/java/io/kuaibao/consumer/controller/UserController.java
+++ dubbo_spring_demo_1/kuaibao-consumer/src/main/java/io/kuaibao/consumer/controller/UserController.java
@@ -2,14 +2,16 @@ package io.kuaibao.consumer.controller;
 
 import io.kuaibao.provider.service.DService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserController {
+
     @Autowired
-    // @Qualifier("d")
+    //@Qualifier("d2")
     private DService service;
 
     @RequestMapping(value = "/test", produces = "application/json; charset=utf-8")
--- dubbo_spring_demo_1/kuaibao-provider/src/main/java/io/kuaibao/provider/service/impl/DServiceImpl_1.java
+++ dubbo_spring_demo_1/kuaibao-provider/src/main/java/io/kuaibao/provider/service/impl/DServiceImpl_1.java
@@ -1,11 +1,12 @@
 package io.kuaibao.provider.service.impl;
 
 import io.kuaibao.provider.service.DService;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by tianwei on 2017/1/21.
  */
-//@Service("d")
+@Service("d1")
 public class DServiceImpl_1 implements DService {
     public String getUserName() {
         return "UserName(用户名)-本机, DServiceImpl_1 i am provider ??";
