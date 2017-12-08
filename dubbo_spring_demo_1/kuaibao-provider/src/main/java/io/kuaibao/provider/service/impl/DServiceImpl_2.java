a896a4e3cbe386e5f1ac0400ead96abb1a8fb30b
shsun <shsun@shsuns-MacBook-Pro.local>
2017-12-08 21:11:41 +0800

    init

--- dubbo_spring_demo_1/kuaibao-provider/src/main/java/io/kuaibao/provider/service/impl/DServiceImpl_2.java
+++ dubbo_spring_demo_1/kuaibao-provider/src/main/java/io/kuaibao/provider/service/impl/DServiceImpl_2.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 /**
  * Created by tianwei on 2017/1/21.
  */
-@Service("d2")
+//@Service("d2")
 public class DServiceImpl_2 implements DService {
     public String getUserName() {
         return "UserName(用户名)-本机, DServiceImpl_2 i am provider ??";
--- dubbo_spring_demo_1/kuaibao-provider/src/main/java/io/kuaibao/provider/service/impl/DServiceImpl_3.java
+++ dubbo_spring_demo_1/kuaibao-provider/src/main/java/io/kuaibao/provider/service/impl/DServiceImpl_3.java
@@ -7,7 +7,7 @@ import io.kuaibao.provider.service.DService;
 /**
  * Created by tianwei on 2017/1/21.
  */
-@Service("d3")
+//@Service("d3")
 public class DServiceImpl_3 implements DService {
     public String getUserName() {
         return "UserName(用户名)-本机, DServiceImpl_3 i am provider ??";
