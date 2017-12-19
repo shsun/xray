e9186bfb7a9f06b0fee1b136f129db6682131049
shsun <89090125@qq.com>
2017-12-19 14:52:55 +0800

    add request&response

--- x/x-common/src/main/java/org/ibase4j/core/base/BaseProviderImpl.java
+++ x/x-common/src/main/java/org/ibase4j/core/base/BaseProviderImpl.java
@@ -16,43 +16,58 @@ import com.alibaba.fastjson.JSON;
 import com.esotericsoftware.reflectasm.MethodAccess;
 
 public abstract class BaseProviderImpl implements ApplicationContextAware, BaseProvider {
-	protected static Logger logger = LogManager.getLogger();
-	private ApplicationContext applicationContext;
+    protected static Logger logger = LogManager.getLogger();
+    private ApplicationContext applicationContext;
 
-	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-		this.applicationContext = applicationContext;
-	}
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
 
-	public Parameter execute(Parameter parameter) {
-		logger.info("请求：{}", JSON.toJSONString(parameter));
-		Object service = applicationContext.getBean(parameter.getService());
-		try {
-			Long id = parameter.getId();
-			BaseModel model = parameter.getModel();
-			List<?> list = parameter.getList();
-			Map<?, ?> map = parameter.getMap();
-			Object result = null;
-			MethodAccess methodAccess = MethodAccess.get(service.getClass());
-			if (id != null) {
-				result = methodAccess.invoke(service, parameter.getMethod(), parameter.getId());
-			} else if (model != null) {
-				result = methodAccess.invoke(service, parameter.getMethod(), parameter.getModel());
-			} else if (list != null) {
-				result = methodAccess.invoke(service, parameter.getMethod(), parameter.getList());
-			} else if (map != null) {
-				result = methodAccess.invoke(service, parameter.getMethod(), parameter.getMap());
-			} else {
-				result = methodAccess.invoke(service, parameter.getMethod());
-			}
-			if (result != null) {
-				Parameter response = new Parameter(result);
-				logger.info("响应：{}", JSON.toJSONString(response));
-				return response;
-			}
-			logger.info("空响应");
-			return null;
-		} catch (Exception e) {
-			throw new BusinessException(Constants.EXCEPTION_HEAD + ExceptionUtil.getStackTraceAsString(e), e);
-		}
-	}
+    public Parameter execute(Parameter p) {
+        logger.info("请求：{}", JSON.toJSONString(p));
+        Object service = applicationContext.getBean(p.getService());
+        try {
+            final long start = System.currentTimeMillis();
+            Object arg = null;
+            Object result = null;
+
+            final Long id = p.getId();
+            final BaseModel model = p.getModel();
+            final List<?> list = p.getList();
+            final Map<?, ?> map = p.getMap();
+
+            MethodAccess access = MethodAccess.get(service.getClass());
+
+            if (id != null) {
+                arg = p.getId();
+                result = access.invoke(service, p.getMethod(), arg);
+            } else if (model != null) {
+                arg = p.getModel();
+                result = access.invoke(service, p.getMethod(), arg);
+            } else if (list != null) {
+                arg = p.getList();
+                result = access.invoke(service, p.getMethod(), arg);
+            } else if (map != null) {
+                arg = p.getMap();
+                result = access.invoke(service, p.getMethod(), arg);
+            } else {
+                arg = null;
+                result = access.invoke(service, p.getMethod());
+            }
+            //
+            final long end = System.currentTimeMillis();
+			System.err.println("Time: " + (end - start) + " ms, " + p.getService() + "." + p.getMethod() + " argument=" + arg);
+
+            if (result != null) {
+                Parameter response = new Parameter(result);
+                logger.info("响应：{}", JSON.toJSONString(response));
+                return response;
+            }
+            logger.info("空响应");
+            return null;
+        } catch (Exception e) {
+            throw new BusinessException(Constants.EXCEPTION_HEAD + ExceptionUtil.getStackTraceAsString(e), e);
+        }
+
+    }
 }
