d8094652c128c76a6217b177004be11d7c36ff35
shsun <89090125@qq.com>
2017-12-21 00:43:47 +0800

    call

--- x/x-common/src/main/java/org/ibase4j/service/TaskFireLogService.java
+++ x/x-common/src/main/java/org/ibase4j/service/TaskFireLogService.java
@@ -1,6 +1,7 @@
 package org.ibase4j.service;
 
 import org.ibase4j.mapper.TaskFireLogMapper;
+import org.ibase4j.model.TaskFireLog;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,5 +10,9 @@ public class TaskFireLogService {
 
     @Autowired
     TaskFireLogMapper taskFireLogMapper;
+
+    public TaskFireLog selectById(Long id) {
+        return taskFireLogMapper.selectById(id);
+    }
 
 }
---
+++ x/x-web/src/main/java/org/ibase4j/web/ATestController.java
@@ -0,0 +1,29 @@
+package org.ibase4j.web;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.ibase4j.model.TaskFireLog;
+import org.ibase4j.service.TaskFireLogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/test")
+public class ATestController {
+
+    @Autowired
+    TaskFireLogService taskFireLogService;
+
+    @RequestMapping("/test_1")
+    @ResponseBody
+    public String test_1(HttpServletRequest request, HttpServletResponse response, ModelMap modelMap) {
+        TaskFireLog log = taskFireLogService.selectById(1L);
+        System.out.println("done");
+        String s = log != null ? log.toString() : "no-data";
+        return s;
+    }
+}
