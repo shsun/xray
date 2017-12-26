3c92b0c6856c96faca88ee4e6f6aae3fcc1af74a
shsun <89090125@qq.com>
2017-12-26 16:25:16 +0800

    init

--- openresty/lua/access.lua
+++ openresty/lua/access.lua
@@ -1,3 +1,7 @@
+if ngx.var.request_uri ~= ngx.var.constant_location then
+    return constant_default_upstream;
+end
+
 local upstream_addr_str = nil;
 if nil ~= ngx.var.upstream_addr then
     upstream_addr_str = " from "..ngx.var.upstream_addr;
--- openresty/lua/rewrite.lua
+++ openresty/lua/rewrite.lua
@@ -3,6 +3,9 @@
 下面的 规则删除了时间戳参数，使得Nginx可以缓存upstream server的响应内容，减轻后端服务器的负载。
 ]]
 
+
+
+
 if ngx.var.args ~= nil then
     -- /some_request?_=1346491660 becomes /some_request
     local fixed_args, count = ngx.re.sub( ngx.var.args, "&?_=[0-9]+", "" );
@@ -11,6 +14,10 @@ if ngx.var.args ~= nil then
     end
 end
 
+
+if ngx.var.request_uri ~= ngx.var.constant_location then
+    return constant_default_upstream;
+end
 
 local upstream_addr_str = nil;
 if nil ~= ngx.var.upstream_addr then
