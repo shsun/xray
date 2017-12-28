77ec1ceb4e381958b98e1e2dbacbad405977e2c1
shsun <89090125@qq.com>
2017-12-28 12:39:18 +0800

    add new server for testing

--- install_all_4_ubuntu_linux.sh
+++ install_all_4_ubuntu_linux.sh
@@ -37,7 +37,8 @@ if [ $input == "Y" ]; then
 	wget -qO - https://openresty.org/package/pubkey.gpg | sudo apt-key add -;
 	sudo apt-get -y install software-properties-common;
 	sudo add-apt-repository -y "deb http://openresty.org/package/ubuntu $(lsb_release -sc) main";
-	sudo apt-get update;sudo apt-get install openresty;
+	sudo apt-get update;
+	sudo apt-get install openresty;
 	echo 'done';
 else
    	echo "$input"
