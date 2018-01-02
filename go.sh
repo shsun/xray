0e75d1e1488c42bc7dd586ab66aa6957802241de
shsun <89090125@qq.com>
2018-01-02 09:18:19 +0800

    cfg

--- go.sh
+++ go.sh
@@ -1,23 +1,28 @@
 #!/bin/sh
 
-
 echo ""
 echo "starting..................... nginx"
 #sudo redis-cli shutdown
 #sudo nginx -s stop;
 sudo nginx -s quit;
 sudo nginx -s reload;
-sleep 3;
+sleep 1;
 
 
 
 echo ""
 echo "starting..................... zookeeper"
-sudo bash /usr/local/share/zookeeper-3.4.6/bin/zkServer.sh stop;
-sleep 3;
-sudo bash /usr/local/share/zookeeper-3.4.6/bin/zkServer.sh start;
-sudo bash /usr/local/share/zookeeper-3.4.6/bin/zkServer.sh status;
-sleep 3;
+ZOOKEEPER_HOME=/usr/local/zookeeper-3.4.6;
+sudo cp -rfv zoo.cfg ${ZOOKEEPER_HOME}/conf/;
+
+sudo bash ${ZOOKEEPER_HOME}/bin/zkServer.sh stop;
+sleep 1;
+echo '';
+sudo bash ${ZOOKEEPER_HOME}/bin/zkServer.sh start;
+sleep 1;
+echo '';
+sudo bash ${ZOOKEEPER_HOME}/bin/zkServer.sh status;
+sleep 1;
 
 
 
@@ -28,3 +33,6 @@ sleep 1;
 sudo cp -rfv redis.conf /opt/;
 sudo redis-server /opt/redis.conf;
 sleep 1;
+echo '';
+
+
