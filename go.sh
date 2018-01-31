6879b1e69d76a48af43193c7a7a51b73b06df41f
shsun <89090125@qq.com>
2018-01-31 16:49:07 +0800

    add 1 redis node

--- go.sh
+++ go.sh
@@ -30,14 +30,16 @@ echo "starting..................... redis"
 sudo redis-cli -p 6379 shutdown;
 sleep 1;
 sudo redis-cli -p 6380 shutdown;
-
 sleep 1;
+sudo redis-cli -p 6381 shutdown;
+sleep 1;
+
 sudo cp -rfv redis/*.conf /opt/;
 
 sudo redis-server /opt/redis_6379.conf;
 sudo redis-server /opt/redis_6380.conf;
+sudo redis-server /opt/redis_6381.conf;
 
 sleep 1;
 echo '';
-
 
